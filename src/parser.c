0f41a229c41000255770409ea41a747ed474f6f4
ltabis <ltabis.pro@gmail.com>
2023-01-22 21:55:27 +0100

    feat: create a basic polish notation parser.

--- src/parser.c
+++ src/parser.c
@@ -3,26 +3,51 @@
 
 // enable flycheck https://www.reddit.com/r/emacs/comments/audffp/tip_how_to_use_a_stable_and_fast_environment_to/
 
-int main(int argc, char **argv) {
-  return 0;
+// Polish Notation Parser.
+typedef struct pnp_s {
+  mpc_parser_t *number;
+  mpc_parser_t *operator;
+  mpc_parser_t *expression;
+  mpc_parser_t *program;
+} pnp_t;
+
+pnp_t init_polish_notation_parser() {
+
+  mpc_parser_t *number = mpc_new("nb");
+  mpc_parser_t *operator = mpc_new("op");
+  mpc_parser_t *expression = mpc_new("expr");
+  mpc_parser_t *program = mpc_new("dlisp");
+
+  pnp_t parser = {
+  .number = number,
+  .operator = operator,
+  .expression = expression,
+  .program = program
+};
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "\
+nb : /-?[0-9]+/ ;                  \
+op : '+' | '-' | '*' | '/' ;       \
+expr : <nb> | '(' <op> <nb>+ ')' ; \
+dlisp : /^/ <op> <expr>+ /$/ ;     \
+",
+            number,
+            operator,
+            expression,
+            program
+);
+
+    return parser;
 }
 
-mpc_parser_t *doge_parser() {
-  mpc_parser_t *adjectives = mpc_or(4,
-                                    mpc_sym("wow"),
-                                    mpc_sym("many"),
-                                    mpc_sym("so"),
-                                    mpc_sym("such")
-                                    );
-  mpc_parser_t *nouns = mpc_or(5,
-                               mpc_sym("lisp"),
-                               mpc_sym("languages"),
-                               mpc_sym("book"),
-                               mpc_sym("build"),
-                               mpc_sym("c")
-                               );
-  mpc_parser_t *phrase = mpc_and(2, mpcf_strfold, adjectives, nouns, free);
-  mpc_parser_t *doge = mpc_many(mpcf_strfold, phrase);
+void cleanup_polish_notation_parser(pnp_t *pnp) {
+  mpc_cleanup(4, pnp->number, pnp->operator, pnp->expression, pnp->program);
+}
 
-  return doge;
+int main(int argc, char **argv) {
+  pnp_t pnp = init_polish_notation_parser();
+
+  cleanup_polish_notation_parser(&pnp);
+  return 0;
 }
