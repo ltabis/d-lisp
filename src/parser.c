db0d240ca2ba2dc0b84e189354b261f8e314514e
ltabis <ltabis.pro@gmail.com>
2023-01-22 20:20:18 +0100

    chore: rename file.

---
+++ src/parser.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  return 0;
+}
+
+mpc_parser_t *doge_parser() {
+    mpc_parser_t *adjectives = mpc_or(4,
+                                    mpc_sym("wow"),
+                                    mpc_sym("many"),
+                                    mpc_sym("so"),
+                                    mpc_sym("such")
+                                );
+  mpc_parser_t *nouns = mpc_or(5,
+                               mpc_sym("lisp"),
+                               mpc_sym("languages"),
+                               mpc_sym("book"),
+                               mpc_sym("build"),
+                               mpc_sym("c"),
+                               );
+  mpc_parser_t *phrase = mpc_and(2, mpcf_strfold, adjectives, nouns, free);
+  mpc_parser_t *doge = mpc_many(mpcf_strfold, phrase);
+
+}
